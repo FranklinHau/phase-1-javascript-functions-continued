ae39867208d6edfa98f2f76c874baf02a2bbc148
Franklin Hau <fhauchi@gmail.com>
2023-03-17 00:35:34 -0600

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+
+}
+saturdayFun();
+
+const mondayWork = function (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+function wrapAdjective (stupid = "*"){
+    const part1 = function(stupid2 = 'special'){
+    return `You are ${stupid}${stupid2}${stupid}!`
+};
+    return part1;
+}
